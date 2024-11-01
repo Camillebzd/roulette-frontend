775efd5073da005c87ad75356da05ef5502c9e0d
Camillebzd <bcamille99@gmail.com>
2024-11-01 11:01:16 +0000

    [ADD] basic roulette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,51 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useActiveAccount } from "thirdweb/react";
-import { Text } from "@chakra-ui/react";
+import { Wheel } from 'react-custom-roulette';
+import { useState } from "react";
+import { Box, Button } from "@chakra-ui/react";
+
+const data = [
+  { option: 'USDC', style: {backgroundColor: '#2588e6'} },
+  { option: 'WETH', style: {backgroundColor: '#6f25e6'}  },
+  { option: 'USDC', style: {backgroundColor: '#2588e6'}  },
+  { option: 'WETH', style: {backgroundColor: '#6f25e6'}  },
+  { option: 'USDC', style: {backgroundColor: '#2588e6'}  },
+  { option: 'WETH', style: {backgroundColor: '#6f25e6'}  },
+  { option: 'USDC', style: {backgroundColor: '#2588e6'}  },
+  { option: 'WETH', style: {backgroundColor: '#6f25e6'}  },
+  { option: 'USDC', style: {backgroundColor: '#2588e6'}  },
+  { option: 'WETH', style: {backgroundColor: '#6f25e6'}  },
+];
 
 export default function Home() {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  const handleSpinClick = () => {
+    const newPrizeNumber = Math.floor(Math.random() * data.length);
+    setPrizeNumber(newPrizeNumber);
+    setMustSpin(true);
+  };
   const acc = useActiveAccount();
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1 className={styles.h1}>Spin the roulette!</h1>
-        <p className={styles.p}>{acc?.address}</p>
-        
+        {/* <p className={styles.p}>{acc?.address}</p> */}
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={data}
+          onStopSpinning={() => {
+            setMustSpin(false);
+          }}
+        />
+        <div>
+          <Button onClick={handleSpinClick}>SPIN</Button>
+        </div>
       </main>
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
@@ -57,7 +90,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
