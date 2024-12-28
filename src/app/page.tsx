26f243cc9aef38218a34139a9191c76e94872264
Camillebzd <bcamille99@gmail.com>
2024-12-28 17:24:38 +0000

    [ADD] first version of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,63 @@
 "use client"
 
+import Image from "next/image";
 import styles from "./page.module.css";
-import { Text } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+
+  const card = (title: string, text: string, buttonTitle: string, onClick: () => any) => {
+    return (
+      <Card display={'flex'} alignItems={'center'}>
+        <CardHeader>
+          <Heading size='3xl'>{title}</Heading>
+        </CardHeader>
+        <CardBody textAlign="center">
+          <Text>{text}</Text>
+        </CardBody>
+        <CardFooter>
+          <Button colorScheme="yellow" onClick={onClick}>{buttonTitle}</Button>
+        </CardFooter>
+      </Card>
+    );
+  };
 
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1 className={styles.h1}>Capy gang</h1>
-        <Text>Blabla around Etherlink and CAPY NFT/group.</Text>
-      </main>
+    <div>
+      {/* <Image src="/casino_background.jpg" alt="Casino background" /> */}
+      <Box position="relative" height="80vh" width="100vw" overflow="hidden">
+        <Image
+          src="/casino_background.jpg"
+          alt="Casino Background"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+        <Box
+          position="absolute"
+          top="30%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          bg="rgba(0, 0, 0, 0.6)"
+          p={1}
+          rounded='md'
+          textAlign="center"
+          backdropFilter="blur(6px)"
+        >
+          <Heading size="4xl" mb={4}>CAPY Gambling</Heading>
+          <Text fontSize="xl">Experience blockchain-based gambling on Etherlink with no fees for CAPY NFT holders!</Text>
+          <Button colorScheme="yellow" size="lg" mt={4} rounded='full'>Start Playing</Button>
+        </Box>
+      </Box>
+      <Flex justifyContent="center" mt={8}>
+        <SimpleGrid spacing={4} columns={[1, 2, 3]} width="70%">
+          {card("Roullink", "Roulette game on Etherlink using on chain random system for fair and amayzing rewards!", "Play here", () => {router.push('/roullink')})}
+          {card("Coinflink", "Using on chain random system, flip coins and double your XTZ!", "Play here", () => {router.push('/coinflink')})}
+          {card("Vault", "New vault system in comming...", "Discover soon", () => {router.push('/vault')})}
+        </SimpleGrid>
+      </Flex>
     </div>
   );
 }
